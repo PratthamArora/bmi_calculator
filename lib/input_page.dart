4831ad399d9f726be6be11596e834c2c919aca0d
PratthamArora <arora.prattham@gmail.com>
2020-03-26 22:25:43 +0530

    added button pressed function

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -132,6 +132,11 @@ class _InputPageState extends State<InputPage> {
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: <Widget>[
                             RoundIconButton(
+                              onClick: () {
+                                setState(() {
+                                  weight--;
+                                });
+                              },
                               child: FontAwesomeIcons.minus,
                             ),
                             SizedBox(
@@ -139,6 +144,11 @@ class _InputPageState extends State<InputPage> {
                             ),
                             RoundIconButton(
                               child: FontAwesomeIcons.plus,
+                              onClick: () {
+                                setState(() {
+                                  weight++;
+                                });
+                              },
                             ),
                           ],
                         ),
@@ -168,21 +178,22 @@ class _InputPageState extends State<InputPage> {
 
 class RoundIconButton extends StatelessWidget {
   final IconData child;
+  final Function onClick;
 
-  RoundIconButton({this.child});
+  RoundIconButton({@required this.child, @required this.onClick});
 
   @override
   Widget build(BuildContext context) {
     return RawMaterialButton(
       child: Icon(child),
-      elevation: 6.0,
+      elevation: 0.0,
       constraints: BoxConstraints.tightFor(
         width: 56.0,
         height: 56.0,
       ),
       shape: CircleBorder(),
       fillColor: Color(0xFF4C4F5E),
-      onPressed: () {},
+      onPressed: onClick,
     );
   }
 }
